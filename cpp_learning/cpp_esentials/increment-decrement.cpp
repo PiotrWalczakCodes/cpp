509d893b163c41fc8ee4baf74b711e903ff3661b
PiotrWalczakCodes <piotrwalczak.pv@gmail.com>
2022-06-24 15:51:02 +0200

    new files

--- cpp_learning/cpp_esentials/increment-decrement.cpp
+++ cpp_learning/cpp_esentials/increment-decrement.cpp
@@ -3,5 +3,32 @@
 using namespace std;
 
 int main(){
+    int var1{1};
+    var1++; // postfix incrementation
+    ++var1; // prefix incrementation
+
+    cout << var1 << endl;
+
+    var1--; // postfix decrementation
+    --var1; // prefix decrementation
+
+    cout << var1 << endl;
+
+    // differences between prefix & postfix
+
+    int result{0}, counter{10};
+    result = ++counter;
+
+    cout << "result: " << result << endl;
+    cout << "counter: " << counter << endl;
+
+
+    result = 0;
+    counter = 10;
+
+    result = counter++;
+    cout << "result: " << result << endl;
+    cout << "counter: " << counter << endl;
+
     return 0;
 }
