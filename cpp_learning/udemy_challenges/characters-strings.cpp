64b65a333204d60b3637905d45e5c77c601af241
PiotrWalczakCodes <piotrwalczak.pv@gmail.com>
2022-07-09 19:24:50 +0200

    new files

--- cpp_learning/udemy_challenges/characters-strings.cpp
+++ cpp_learning/udemy_challenges/characters-strings.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 int main(){
-    // !code here
+    string alphabet{"abcdefghijklmnoprstuwxyz"};
+    string key{"zyxwutsrponmlkjihgfedcba"};
+
+    string message{};
+    getline(cin, message);
+
+    string encrypted_message{};
+    for (auto i : message)  {
+        if (i == ' ' || !isalpha(i)) {
+            encrypted_message += i;
+        } else {
+            encrypted_message += key[alphabet.find(tolower(i))];
+        }
+    }
+    cout << "\n\n\n\n\n\n\n\n\n\n";
+    cout << encrypted_message << endl;
     return 0;
 }
